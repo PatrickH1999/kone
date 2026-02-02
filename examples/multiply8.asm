75555404f56ac5e53a9ac4e418e9a6c615e6b577
PatrickHermsmeyer <patrickhermsmeyer@web.de>
2026-02-02 14:18:09 +0100

    multiplication is now fully working, except that it occasionally randomly fails which needs to be investigated.

--- examples/multiply8.asm
+++ examples/multiply8.asm
@@ -1,23 +1,28 @@
-INN 0
-INN 1
+INN 0     // input left (R0)
+INN 1     // input right (R1)
 LDI 0
-STR 2
+//
+STR 2     // output (R2)
 LDI 248
-STR 3
-LDI 1
+//
+STR 3     // bit counter (starts at 248, ends at 0) (R3)
+LDI 1     // LOOP
+//
 AND 1
-JA0 11
+JA0 16    // jump to CONTINUE (line 16) if first digit in R1 is 0, ELSE:
+//
 LDR 2
 ADD 0
-STR 2
-LDR 0
+STR 2     // R2 = R2 + R0
+LDR 0     // CONTINUE
 BSL
-STR 0
+STR 0     // R0 bitshift left (multiply by one order)
 LDR 1
 BSR
-STR 1
-OUT 2
+STR 1     // R1 bitshift right
 LDI 1
+//
 ADD 3
-STR 3
-JA1 6
+STR 3     // R3 += 1
+JA1 6     // jump to LOOP (line 8) if bit counter is not zero
+OUT 2     // print result
