a63b63875fc0a0485484d0510e00902bfec17744
PatrickHermsmeyer <patrickhermsmeyer@web.de>
2026-07-09 14:02:36 +0200

    Removed scanf function from utility, as it will be replaced by the keyboard device

--- src/utility.c
+++ src/utility.c
@@ -30,26 +30,6 @@ void addr_convert_16_to_8(uint8_t addr8[2], uint16_t addr16) {
     addr8[1] = (uint8_t)(addr16 >> (8 * sizeof(uint8_t)));
 }
 
-int scanf_uint8(uint8_t *out) {
-    char buf[32];
-    char *end;
-    unsigned long tmp;
-
-    if (!out) return 0;
-    if (!fgets(buf, sizeof buf, stdin)) return 0;
-
-    errno = 0;
-    tmp = strtoul(buf, &end, 10);
-
-    if (end == buf) return 0;
-    if (*end != '\n' && *end != '\0') return 0;
-    if (errno || tmp > UINT8_MAX) return 0;
-
-    *out = (uint8_t)tmp;
-
-    return 1;
-}
-
 uint8_t brl8(uint8_t x, unsigned n) {
     n &= 7; // n mod 8
     return (x << n) | (x >> (8 - n));
--- src/utility.h
+++ src/utility.h
@@ -10,7 +10,6 @@ void print_out(CPU *cpu, Display *display, int v);
 void out_cleanup(int sig);
 void addr_convert_8_to_16(uint16_t *addr16, const uint8_t addr8[2]);
 void addr_convert_16_to_8(uint8_t addr8[2], uint16_t addr16);
-int scanf_uint8(uint8_t *out);
 uint8_t brl8(uint8_t x, unsigned n);
 uint8_t brr8(uint8_t x, unsigned n);
 int get_pos_first_1_in_byte(uint8_t byte);
